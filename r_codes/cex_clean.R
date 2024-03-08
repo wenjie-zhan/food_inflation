9e6004cb5caf2b636a25732aea95ce02969c53d7
Wenjie Zhan <wjzhan.ac@gmail.com>
2024-03-08 15:22:06 -0800

    create folders

---
+++ r_codes/cex_clean.R
@@ -0,0 +1,38 @@
+################################################################################
+##### Project: food inflation
+##### Program: cex processing
+##### Coding: Wenjie Zhan
+################################################################################
+# Clean the Environment
+rm(list=ls())
+
+# Loading packages
+library(tidyverse)
+library(tidycensus)
+library(data.table)
+library(dplyr)
+library(ggplot2)
+library(sandwich) # vcov
+library(usmap) # us map with HI and AK
+library(magrittr) # %$%
+library(plotrix) # std.error()
+library(haven) # read_dta()
+
+################################################################################
+##### 1. import CEX data
+################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
